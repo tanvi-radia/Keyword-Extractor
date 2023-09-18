bf2b2f16047009a10cf27be6178feffe3b88bad7
Tanvi Radia <139433835+tanvi-radia@users.noreply.github.com>
2023-09-19 00:15:58 +0530

    Update KeywordsModal.jsx

--- src/components/KeywordsModal.jsx
+++ src/components/KeywordsModal.jsx
@@ -1,5 +1,4 @@
 import { Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, CircularProgress } from '@chakra-ui/react';
-
 const KeywordsModal = ({ keywords, loading, isOpen, closeModal }) => {
   return (
     <>
