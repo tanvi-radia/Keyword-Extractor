bd6781a70614b68f53153c6c8d0bfcc87ffbce07
Tanvi Radia <139433835+tanvi-radia@users.noreply.github.com>
2023-09-19 00:20:42 +0530

    Update TextInput.jsx

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -23,25 +23,8 @@ const TextInput = ({ extractKeywords }) => {
 
   return (
     <>
-      <Textarea
-        bg='blue.400'
-        padding={4}
-        marginTop={6}
-        height={200}
-        color='white'
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-
-      <Button
-        bg='blue.500'
-        color='white'
-        marginTop={4}
-        width='100%'
-        _hover={{ bg: 'blue.700' }}
-        onClick={submitText}>
-        Extract Keywords
-      </Button>
+      <Textarea bg='blue.400' padding={4} marginTop={6} height={200} color='white' value={text} onChange={(e) => setText(e.target.value)} />
+      <Button bg='blue.500' color='white' marginTop={4} width='100%' _hover={{ bg: 'blue.700' }} onClick={submitText}>Extract Keywords</Button>
     </>
   );
 };
